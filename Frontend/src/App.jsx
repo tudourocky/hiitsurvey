aa60e7e6bba8265dc5dcdaccff17b6c39dd16e2c
RileyWong26 <rileywongwong@gmail.com>
2026-01-17 13:51:29 -0500

    Created workout preference and integrated to the workout preference api endpoint

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ArcadeSongSelector from './SurveySelector' 
+import PreferenceForm from './pages/PreferenceForm'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      < ArcadeSongSelector />
+      {/* < ArcadeSongSelector /> */}
+      <PreferenceForm />
     </>
   )
 }
