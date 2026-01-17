1a6d23988c084aff859bf0b4aad57752b277e878
Allan Su <67175442+tudourocky@users.noreply.github.com>
2026-01-17 11:59:59 -0500

    working ok version

--- backend/main.py
+++ backend/main.py
@@ -25,8 +25,8 @@ app.add_middleware(
 )
 
 # Download model file if it doesn't exist
-MODEL_PATH = "pose_landmarker.task"
-MODEL_URL = "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task"
+MODEL_PATH = "pose_landmarker_full.task"
+MODEL_URL = "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/1/pose_landmarker_full.task"
 
 if not os.path.exists(MODEL_PATH):
     try:
