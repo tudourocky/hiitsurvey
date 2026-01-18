e413f7230bb19d3a0530e9ce1be3e019bf3b8e00
RileyWong26 <rileywongwong@gmail.com>
2026-01-18 00:08:32 -0500

    Env example and requirements

--- Frontend/src/pages/SignIn.jsx
+++ Frontend/src/pages/SignIn.jsx
@@ -16,7 +16,9 @@ export default function SignIn() {
         <div className="user-type-toggle">
           <button
             className={`toggle-btn`}
-            onClick={() => {signInWithGoogle('surveyor');} }
+            onClick={() => {
+                signInWithGoogle('surveyor'); 
+            } }
           >
             <svg className="toggle-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
